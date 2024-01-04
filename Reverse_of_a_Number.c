2d3909a78ca5ab0f1d0f50cb46fdd7a1935a425d
sounakkk <151904135+sounakkk@users.noreply.github.com>
2024-01-04 23:45:39 +0530

    #include <stdio.h>
    int main() {
        int num, reversed = 0, remainder;
        scanf("%d", &num);
        while (num != 0) {
            remainder = num % 10;
            reversed = reversed * 10 + remainder;
            num /= 10;
        }
        printf("%d
    ", reversed);
    
        return 0;
    }

---
+++ Reverse_of_a_Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder;
+    scanf("%d", &num);
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+    printf("%d
+", reversed);
+
+    return 0;
+}
